6ab4ac7ca73949fea33e728b5cf07835dccb404f
Erik Suris <erikvsuris@gmail.com>
2023-07-05 14:22:48 -0300

    Keeper pt1

--- frontend/js/react/my-app/src/index.js
+++ frontend/js/react/my-app/src/index.js
@@ -1,12 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './components/App'
 
-const customStyle = {color: 'red'}
-
-ReactDOM.render(
-  <div>
-    <h1 style={customStyle}>Hello, world!</h1>
-    <p>Lorem ipsum dolor upset.</p>
-  </div>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App/>,document.getElementById('root'))
