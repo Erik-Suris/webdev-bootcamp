69c39f76b8a5900b17c021d3ae7b17fb68239077
Erik Suris <erikvsuris@gmail.com>
2023-07-06 20:44:48 -0300

    ES6 Array Functions

--- frontend/js/react/mapping-components/src/components/App.jsx
+++ frontend/js/react/mapping-components/src/components/App.jsx
@@ -2,19 +2,17 @@ import React from "react";
 import Entry from './Entry'
 import emojipedia from '../emojipedia'
 
-function createEntry(emojipedia) {
-  return <Entry
-    key={emojipedia.id}
-    emoji={emojipedia.emoji}
-    name={emojipedia.name}
-    meaning={emojipedia.meaning}
-  />
-}
-
 function App() {
   return <div>    
     <dl className="dictionary">
-      {emojipedia.map(createEntry)}
+      {emojipedia.map(emojipedia => {
+        <Entry
+          key={emojipedia.id}
+          emoji={emojipedia.emoji}
+          name={emojipedia.name}
+          meaning={emojipedia.meaning}
+        />
+      })}
     </dl>
   </div>
 }
