bedfc7124e947c44911fd120370e019c557c8dfd
Erik Suris <erikvsuris@gmail.com>
2023-07-06 21:57:29 -0300

    Keeper pt2

--- frontend/js/react/keeper/src/components/App.jsx
+++ frontend/js/react/keeper/src/components/App.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import Header from './Header'
 import Note from './Note'
 import Footer from './Footer'
+import notes from '../notes'
+
+function createNotes(noteItem) {
+    return <Note title={noteItem.title} content={noteItem.content}/>
+}
 
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map(noteItem =>
+            <Note
+                title={noteItem.title}
+                content={noteItem.content}
+            />)}
             <Footer />
         </div>
     )
